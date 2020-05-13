eb1180f68b000b683eb03e30ee5f14965796392c
Kim Fitter <kimfitter@yahoo.com>
2020-05-14 10:18:33 +1200

    Add pseudocode

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// -    -   - //
+// Calculator //
+// -    -   - //
+
+// Create variable for the calculator object from the html class calculator
+
+// Create object for screen object with default value as null from the html object screen
+
+// Create an running total variable default to 0
+
+// Create a new button array from the html class button with 5x4 buttons, where one button, the + is a bigger button
+
+// Create an array with the sequence of input values as buttons are clicked
+
+// Create an event handler on click for the array buttons, with the button function  buttonClick 
+
+// Create a function buttonClick that does something on each button click 
+
+//      Saves ie push the value of the button to the sequence array then,
+
+//      If it is one of the clear buttons it will clear the screen value, and reset the running total to 0 and break
+
+//      Else if it is the operator button +,-,% or / then break
+
+//      Else if if the button value is 0,1,2,3,4,5,6,7,8, or 9 
+//          it will assigns that value of the button to the screen value 
+//           calculate the running total based on the sequence
+//                  this is a bit complicated...check if the previous button value in the sequence is an operator +,-,% or / 
+//                      then create 4 if statements for each operator, each it will return the total with  a calculation of sequence[i-2] sequence[i-1] sequence [i]
+//              and break
+
+//      Else if it is the operator button = then return the running total value, and reset the running total to 0
+
+
+
+
